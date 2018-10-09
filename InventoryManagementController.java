6d7142b3f53b369332bee1b7d4e346b2cc57a2d8
gauri999 <43775397+gauri999@users.noreply.github.com>
2018-10-09 16:03:13 +1100

    Add files via upload

--- InventoryManagementController.java
+++ InventoryManagementController.java
@@ -9,13 +9,19 @@ import java.util.*;
 public class InventoryManagementController
 {
     // instance variables - replace the example below with your own
-    private int x;
+
+     private int x;
+    ArrayList<String> list;
+    ArrayList<String> searchList;
 
     /**
      * Constructor for objects of class InventoryManagementController
      */
     public InventoryManagementController()
     {
+                list = new ArrayList<String>();
+        searchList = new ArrayList<String>();
+        x = 0;
     }
 
     /**
@@ -26,6 +32,7 @@ public class InventoryManagementController
      */
     public boolean addProduct(String name,String origin,String packaging, double price,double discount,int quantity,int shipmentID)
     {
+       
         Product product = new Product();
         product.setProductID(1);//generate product ID.
         product.setProductName(name);
@@ -55,11 +62,181 @@ public class InventoryManagementController
         return false;
     }
     
-    public boolean searchProduct(String name,String origin)
+    public void readfile()
     {
-        Product p1= new Product();
-        p1.setProductName(name);
-        p1.setOrigin(origin);
-        return true;
+        String filename = ("product.txt");
+        String line = null;
+        String[] lines = null;
+
+        try
+        {
+            /* FileReader reads text files in the default encoding */
+            FileReader fileReader = new FileReader(filename);
+
+            /* always wrap the FileReader in BufferedReader */
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            int i = 0;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                lines = line.split(",");
+                list.add(new String(lines[0]));
+                list.add(new String(lines[1]));
+                //System.out.println(line);
+                i++;
+            }
+            for (i=0;i<list.size();i++)
+               // System.out.println(list.get(i));
+            bufferedReader.close();
+            searchCriteria();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error Reading File");
+        }
+
+    }
+    
+     public void searchCriteria()
+    {
+        //readfile();
+                System.out.println("------------------------------------------------");
+        System.out.println("Enter criteria for searching the product");
+        System.out.println("1.Search by product name");
+        System.out.println("2.Search by product origin");
+        System.out.println("3.Search by product name and origin");
+        System.out.println("Please enter your choice - ");
+        Scanner s1 = new Scanner(System.in); //taking input from console through console object
+        int name = s1.nextInt();
+
+        switch (name)//SWitch case for selecting option
+        {
+            case 1:
+            System.out.println("Enter the product name -");
+            String seq = s1.next();
+            searchByProdName(seq);
+            break;
+            case 2:
+            System.out.println("Enter the product Origin -");
+            String seq1 = s1.next();
+            searchByProdOrigin(seq1);
+            break;
+            case 3:
+            System.out.println("Enter the product name -");
+            String seq2 = s1.next();
+            System.out.println("Enter the product Origin -");
+            String seq3 = s1.next();
+            searchByBoth(seq2,seq3);
+            break;
+            default:
+            System.out.println("Invalid option");
+            break;
+
+        }
+    }
+
+    public void searchByProdName(String seq)
+    {
+        for (int i=0;i<list.size();i++)
+        {
+
+            if(i % 2 == 0)
+            {
+                // System.out.println(list.get(i));
+
+                String test = list.get(i); 
+                boolean bool = (test.toLowerCase()).contains(seq.toLowerCase()); 
+                if(bool == true)
+                    System.out.println(list.get(i)+","+list.get(i+1));
+            }
+            
+            if(i % 2 != 0)
+            {
+                continue;
+            }
+        }
+        searchCriteria();
+    }
+
+    public void searchByProdOrigin(String seq)
+    {
+        for (int i=0;i<list.size();i++)
+        {
+
+            if(i % 2 != 0)
+            {
+                // System.out.println(list.get(i));
+
+                String test = list.get(i); 
+                boolean bool = (test.toLowerCase()).contains(seq.toLowerCase()); 
+                if(bool == true)
+                    System.out.println(list.get(i-1)+","+list.get(i));
+            }
+            
+            if(i % 2 == 0)
+            {
+                continue;
+            }
+        }
+        searchCriteria();
+    }
+    
+    public void searchByBoth(String seq, String seq1)
+    {
+        for (int i=0;i<list.size();i++)
+        {
+
+            //String test = list.get(i); 
+            //boolean bool = (test.toLowerCase()).contains(seq.toLowerCase()); 
+                             //boolean bool1 = (test.toLowerCase()).contains(seq1.toLowerCase()); 
+                             
+             if(i % 2 == 0)
+            {
+                // System.out.println(list.get(i));
+
+                String test = list.get(i); 
+                boolean bool = (test.toLowerCase()).contains(seq.toLowerCase()); 
+                if(bool == true)
+                    System.out.println(list.get(i)+","+list.get(i+1));
+            }
+            
+            if(i % 2 != 0)
+            {
+                continue;
+            }
+        }
+        
+        for (int j=0;j<list.size();j++)
+        {
+            if(j % 2 != 0)
+            {
+                // System.out.println(list.get(i));
+
+                String test = list.get(j); 
+                boolean bool = (test.toLowerCase()).contains(seq1.toLowerCase()); 
+                if(bool == true)
+                    System.out.println(list.get(j-1)+","+list.get(j));
+            }
+            
+            if(j % 2 == 0)
+            {
+                continue;
+            }
+        }                   
+            //boolean bool = (test.toLowerCase()).contains(seq.toLowerCase()); 
+
+ //if(bool == true && i % 2 != 0)
+              //  System.out.println(list.get(i-1)+","+list.get(i));
+            //if(bool == true && i % 2 == 0 )
+              //  System.out.println(list.get(i)+","+list.get(i+1));
+             
+        searchCriteria();
+            
+        }
+
+    
+
+    public void searchProduct()
+    {
+        readfile();
     }
 }
